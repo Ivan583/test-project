89db0c17acda941c092b3f723cbe6767f19601fa
Ivan583 <ivanych2293@gmail.com>
2020-09-15 01:54:07 +0400

    Отключение перезагрузки страниц

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,6 @@
   <div>
     <h2>Home page</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    <a href="/todos">Todos</a>
+    <router-link to="/todos">Todos</router-link>
   </div>
 </template>
