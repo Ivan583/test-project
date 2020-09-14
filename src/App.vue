7f672f4a3411356c72a2d25e88db36027b9f02c2
Ivan583 <ivanych2293@gmail.com>
2020-09-15 01:40:49 +0400

    Подключение роутера

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,11 @@
 <template>
   <div id="app">
     <h1>Todo application</h1>
-    <AddTodo @add-todo="addTodo" />
     <hr />
-    <TodoList :todo="todos" @remove-elem="removeElem" />
+
+    <router-view />
   </div>
 </template>
-
-<script>
-import TodoList from "@/components/TodoList";
-import AddTodo from "@/components/AddTodo";
-export default {
-  name: "App",
-  data() {
-    return {
-      todos: []
-    };
-  },
-  mounted() {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=3")
-      .then(response => response.json())
-      .then(json => {
-        this.todos = json;
-      });
-  },
-  components: {
-    TodoList,
-    AddTodo
-  },
-  methods: {
-    removeElem(id) {
-      this.todos = this.todos.filter(t => t.id != id);
-    },
-    addTodo(todo) {
-      this.todos.push(todo);
-    }
-  }
-};
-</script>
 
 <style>
 #app {
